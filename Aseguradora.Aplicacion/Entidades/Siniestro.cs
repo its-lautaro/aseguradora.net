8b700613766d0be64188a1633f6a566c887f53d8
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-06-05 13:51:08 -0300

    add repo siniestro

--- Aseguradora.Aplicacion/Entidades/Siniestro.cs
+++ Aseguradora.Aplicacion/Entidades/Siniestro.cs
@@ -1,17 +1,7 @@
 namespace Aseguradora.Aplicacion.Entidades;
 
 public class Siniestro{
-    static int s_id = 0;
-    private int _id;
-    public int Id
-    {
-        get { return _id; }
-        private set
-        {
-            _id = value;
-            s_id++;
-        }
-    }
+    public int Id { get; set; }
     public int IdPoliza { get; set; }
     public DateTime Fecha_ingreso { get; set; }
     public DateTime Fecha_incidente { get; set; }
@@ -19,7 +9,7 @@ public class Siniestro{
     public string Descripcion { get; set; }
 
     public Siniestro(int idpoliza, DateTime fecha_ingreso, DateTime fecha_incidente, string direccion, string descripcion){
-        Id=s_id;
+        Id=-1;
         IdPoliza=idpoliza;
         Fecha_ingreso=fecha_ingreso;
         Fecha_incidente=fecha_incidente;
---
+++ Aseguradora.Aplicacion/Interfaces/IRepositorioSiniestro.cs
@@ -0,0 +1,10 @@
+using Aseguradora.Aplicacion.Entidades;
+namespace Aseguradora.Aplicacion.Interfaces;
+
+public interface IRepositorioSiniestro
+{
+    public void AgregarSiniestro(Siniestro s);
+    public void ModificarSiniestro(Siniestro s);
+    public Siniestro EliminarSiniestro(int id);
+    List<Siniestro> ListarSiniestros();
+}
