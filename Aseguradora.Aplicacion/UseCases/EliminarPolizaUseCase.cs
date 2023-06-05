a1639155597e440df48aeceabfd37bd24a63a516
Mai3012 <maite-3012@hotmail.com>
2023-06-05 15:18:23 -0300

    eliminacion cascada poliza

--- Aseguradora.Aplicacion/UseCases/EliminarPolizaUseCase.cs
+++ Aseguradora.Aplicacion/UseCases/EliminarPolizaUseCase.cs
@@ -3,13 +3,27 @@ using Aseguradora.Aplicacion.Entidades;
 using Aseguradora.Aplicacion.Interfaces;
 public class EliminarPolizaUseCase
 {
-    private readonly IRepositorioPoliza _repo;
-    public EliminarPolizaUseCase(IRepositorioPoliza repo)
+    private readonly IRepositorioPoliza _repoP;
+    private readonly IRepositorioSiniestro _repoS;
+
+    public EliminarPolizaUseCase(IRepositorioPoliza repoP, IRepositorioSiniestro repoS)
     {
-        this._repo = repo;
+        this._repoP = repoP;
+        this._repoS = repoS;
     }
     public void Ejecutar(int id)
     {
-        _repo.EliminarPoliza(id);
+        _repoP.EliminarPoliza(id);
+        //puede haber mas de un siniestro por poliza
+        List<Siniestro> siniestros = _repoS.ListarSiniestros();
+        List<int>? indiceS = new List<int>();
+        foreach (Siniestro s in siniestros){
+            if (s.IdPoliza == id){
+                indiceS.Add(s.Id);
+            }
+        }
+        foreach (int i in indiceS){
+            siniestros.RemoveAt(i);
+        }
     }    
 }
