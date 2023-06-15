7a6ef71068ccd520730f4cfc6c248e726f2172eb
Mai3012 <maite-3012@hotmail.com>
2023-06-15 13:56:28 -0300

    Listar, modificar y eliminar titulares

--- Aseguradora.Aplicacion/Entidades/Persona.cs
+++ Aseguradora.Aplicacion/Entidades/Persona.cs
@@ -2,12 +2,15 @@
 
 public abstract class Persona
 {
-    public int? DNI { get; set; }
-    public string? Apellido { get; set; }
-    public string? Nombre { get; set; }
-    public int? Telefono { get; set; }
+    public int DNI { get; set; }
+    public string Apellido { get; set; }
+    public string Nombre { get; set; }
+    public int Telefono { get; set; }
 
     public Persona(){
-
+        this.DNI=-1;
+        this.Apellido="";
+        this.Nombre="";
+        this.Telefono=-1;
     }
 }
--- Aseguradora.Aplicacion/Entidades/Titular.cs
+++ Aseguradora.Aplicacion/Entidades/Titular.cs
@@ -3,14 +3,18 @@ namespace Aseguradora.Aplicacion.Entidades;
 public class Titular : Persona
 {
     public int Id { get; set; }
-    public string? Direccion { get; set; }
-    public string? Correo { get; set; }
+    public string Direccion { get; set; }
+    public string Correo { get; set; }
 
-    public List<Vehiculo>? Vehiculos{
+    public List<Vehiculo> Vehiculos{
         get;
         set;
     }
-    public Titular(){}
+    public Titular(){
+        this.Direccion="";
+        this.Correo="";
+        Vehiculos= new List<Vehiculo>();
+    }
 
     public override string ToString()
     {
