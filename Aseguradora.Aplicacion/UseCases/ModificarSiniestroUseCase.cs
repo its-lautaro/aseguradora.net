96b71182c9e109692baff097118cbaff6a4cdb82
its-lautaro <lautaro.lavecchia@hotmail.com>
2023-06-16 16:27:03 -0300

    fix modificar siniestro

--- Aseguradora.Aplicacion/UseCases/ModificarSiniestroUseCase.cs
+++ Aseguradora.Aplicacion/UseCases/ModificarSiniestroUseCase.cs
@@ -4,12 +4,33 @@ using Aseguradora.Aplicacion.Interfaces;
 public class ModificarSiniestroUseCase
 {
     private readonly IRepositorioSiniestro _repo;
-    public ModificarSiniestroUseCase(IRepositorioSiniestro repo)
+    private ListarPolizasUseCase listarPolizas;
+    public ModificarSiniestroUseCase(IRepositorioSiniestro repo, IRepositorioPoliza repoP)
     {
         this._repo = repo;
+        listarPolizas = new ListarPolizasUseCase(repoP);
     }
     public void Ejecutar(Siniestro siniestro)
     {
-        _repo.ModificarSiniestro(siniestro);
-    }    
+        List<Poliza> polizas = listarPolizas.Ejecutar();
+        DateTime? fechaI = null, fechaF = null;
+        foreach (Poliza p in polizas)
+        {
+            if (p.Id == siniestro.PolizaId)
+            {
+                fechaI = p.Fecha_inicio;
+                fechaF = p.Fecha_fin;
+                break;
+            }
+        }
+        if (fechaI < siniestro.Fecha_incidente && fechaF > siniestro.Fecha_incidente)
+        {
+            _repo.ModificarSiniestro(siniestro);
+        }
+        else
+        {
+            throw new Exception("El titular no cuenta con cobertura vigente");
+        }
+        
+    }
 }
